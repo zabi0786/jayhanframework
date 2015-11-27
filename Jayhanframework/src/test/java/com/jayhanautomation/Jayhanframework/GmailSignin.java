6c675cc15eeaa1b2021098b26d9c21fbea4b2918
zabi0786 <Z_A_B_I_786@yahoo.com>
2015-11-27 15:20:53 -0500

    2nd commit

--- Jayhanframework/src/test/java/com/jayhanautomation/Jayhanframework/GmailSignin.java
+++ Jayhanframework/src/test/java/com/jayhanautomation/Jayhanframework/GmailSignin.java
@@ -33,34 +33,38 @@ public class GmailSignin {
 		
 //		
 ////		3.Fill in Password
-		WebElement PasswordTxtbox=driver.findElement(By.id("Passwd"));
+		WebElement PasswordTxtbox=driver.findElement(By.name("Passwd"));
 		PasswordTxtbox.clear();
 		PasswordTxtbox.sendKeys("S1mil@re");
-//				
-////		4.Click Singin
-		driver.findElement(By.id("signIn")).click();
-//		
-////		5.Verify User did sign in.
-		WebDriverWait wait = new WebDriverWait(driver,30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Inbox")));
-		Assert.assertTrue("Inbox should exist",driver.findElements(By.partialLinkText("Inbox")).size()>0);
 		
-
-////		6.Signout
-		WebElement profilebutton= driver.findElement(By.cssSelector(".gb_Ka.gbii"));
-		profilebutton.click();
-		WebElement Signoutlink= driver.findElement(By.id("gb_71"));
-		Signoutlink.click();
-////		7.Verfird user did signout
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signIn")));
-		Assert.assertTrue("signIn should exist",driver.findElements(By.id("signIn")).size()>0);
-		 
-	}
+				
+//		4.Click Singin
+			driver.findElement(By.id("signIn")).click();
 	
+//		5.Verify User did sign in.
+			WebDriverWait wait = new WebDriverWait(driver,30);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Inbox")));
+			Assert.assertTrue("Inbox should exist",driver.findElements(By.partialLinkText("Inbox")).size()>0);
+
+//		5.1 Email Compose
+			WebElement composemail=driver.findElement(By.xpath(".//*[@id=':it']/div/div"));
+			composemail.click();
+			
+//		6.Signout
+			WebElement profilebutton= driver.findElement(By.cssSelector(".gb_Ka.gbii"));
+			profilebutton.click();
+			WebElement Signoutlink= driver.findElement(By.id("gb_71"));
+			Signoutlink.click();
+//		7.Verfird user did signout
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signIn")));
+			Assert.assertTrue("signIn should exist",driver.findElements(By.id("signIn")).size()>0);
+		 
+}
+
 	@After
 	public void teardown(){
 		driver.quit();
-		
+	
 	}
 
 }
